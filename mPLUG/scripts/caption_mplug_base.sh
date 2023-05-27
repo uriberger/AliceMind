a30a57f7a56f9a9b3e2bcec7c281f1d37d8d8937
Uri Berger <uriber@cs.huji.ac.il>
2023-05-27 16:25:53 +0300

    Changed the caption training script

--- mPLUG/scripts/caption_mplug_base.sh
+++ mPLUG/scripts/caption_mplug_base.sh
@@ -1,14 +1,8 @@
-#pip install transformers==4.8.1
-pip install colorlog
-apt-get update
-apt-get install default-jdk
-apt-get install default-jre
-pip install git+git://github.com/j-min/language-evaluation@master
-python -c "import language_evaluation; language_evaluation.download('coco')"
+#!/bin/sh
 
 for lr in 1e-5
 do
-    CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=3224  --use_env caption_mplug.py \
+    CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=4 --master_port=3224  --use_env caption_mplug.py \
     --config ./configs/caption_mplug_base.yaml \
     --output_dir output/coco_caption_base_$lr \
     --checkpoint ./mplug_base.pth \
