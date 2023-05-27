ee5507a1ac7afc97c932b1bbfcccca994032e054
Uri Berger <uriber@cs.huji.ac.il>
2023-05-27 23:32:31 +0300

    Changed encoder and decoder to huggingface's default bert

--- mPLUG/scripts/caption_mplug_base.sh
+++ mPLUG/scripts/caption_mplug_base.sh
@@ -6,8 +6,8 @@ do
     --config ./configs/caption_mplug_base.yaml \
     --output_dir output/coco_caption_base_$lr \
     --checkpoint ./mplug_base.pth \
-    --text_encoder /nas-alinlp/lcl193798/albef/google/bert-base-uncased \
-    --text_decoder /nas-alinlp/lcl193798/albef/google/bert-base-uncased \
+    --text_encoder bert-base-uncased \
+    --text_decoder bert-base-uncased \
     --do_two_optim \
     --lr $lr \
     --min_length 8 \
