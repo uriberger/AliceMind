73c64c76ea4f55c77aa3b98d62e791a7f5321ec4
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-06-25 16:52:38 +0300

    Added code to create local ann file

---
+++ mPLUG/dataset/create_local_ann_file.py
@@ -0,0 +1,24 @@
+import sys
+import json
+import random
+
+assert len(sys.argv) == 2
+image_ids_file = sys.argv[1]
+with open(image_ids_file, 'r') as fp:
+    image_id_list = json.load(fp)
+
+ann_data = []
+
+with open('../../CLIP_prefix_caption/dataset_coco.json', 'r') as fp:
+    all_coco_data = json.load(fp)['images']
+
+for image_id in image_id_list:
+    sample = [x for x in all_coco_data if x['cocoid'] == image_id][0]
+    caption_inds = random.sample(range(len(sample['sentences'])), 2)
+    res = sample
+    res['sentences'] = [res['sentences'][i] for i in range(caption_inds)]
+    ann_data.append(res)
+
+with open('ann.json', 'w') as fp:
+    fp.write(json.dumps(ann_data))
+
