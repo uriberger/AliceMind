ef4d4023cb7bc3e829ea5c6d62ee8a72b3991569
Uri Berger <uriber@cs.huji.ac.il>
2023-06-26 14:04:46 +0300

    Changed config and script to match reformulation project

--- mPLUG/scripts/caption_mplug_base.sh
+++ mPLUG/scripts/caption_mplug_base.sh
@@ -4,7 +4,7 @@ for lr in 1e-5
 do
     CUDA_VISIBLE_DEVICES=0 venv/bin/python caption_mplug.py \
     --config ./configs/caption_mplug_base.yaml \
-    --output_dir output/coco_caption_base_$lr \
+    --output_dir output/after_batch_1 \
     --checkpoint ./mplug_base.pth \
     --text_encoder bert-base-uncased \
     --text_decoder bert-base-uncased \
