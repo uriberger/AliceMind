54ce00957d1b748cebe7d1bdd2ef945d73cb008f
Uri Berger <uriber@cs.huji.ac.il>
2023-07-07 01:00:13 +0300

    Fixed some bugs in the refomulate script

--- mPLUG/reformulate.py
+++ mPLUG/reformulate.py
@@ -3,15 +3,18 @@ import os
 import yaml
 import json
 import torch
+import torch.nn as nn
 from torchvision import transforms
 from PIL import Image
 from models.model_vqa_mplug import MPLUG
 from models.tokenization_bert import BertTokenizer
+from models.vit import resize_pos_embed
 
 assert len(sys.argv) == 3
 
 model_path = sys.argv[1]
-data = sys.argv[2]
+with open(sys.argv[2], 'r') as fp:
+    data = json.load(fp)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 config = yaml.load(open('configs/vqa_mplug_base.yaml', 'r'), Loader=yaml.Loader)
@@ -25,6 +28,9 @@ config['text_decoder'] = 'bert-base-uncased'
 
 print("Creating model")
 model = MPLUG(config=config, tokenizer=tokenizer)
+checkpoint = torch.load(model_path, map_location='cpu')
+state_dict = checkpoint['model']
+model.load_state_dict(state_dict, strict=False)
 model.eval()
 device = torch.device('cuda')
 model = model.to(device)
@@ -44,11 +50,13 @@ for sample in data:
     image = Image.open(image_path).convert('RGB')
     image = transform(image)
     image = image.to(device, non_blocking=True)
+    image = image.unsqueeze(0)
 
     question = sample['caption']
     question_input = tokenizer(question, padding='longest', return_tensors="pt").to(device)
-    result = model(image, question_input, answer=None, train=False, k=config['k_test'])
-    res.append({'cocoid': image_id, 'sentences': [result]})
+    topk_ids, topk_probs = model(image, question_input, answer=None, train=False, k=config['k_test'])
+    ans = tokenizer.decode(topk_ids[0][0]).replace("[SEP]", "").replace("[CLS]", "").replace("[PAD]", "").strip()
+    res.append({'cocoid': image_id, 'sentences': [ans]})
 
 with open('ann.json', 'w') as fp:
     fp.write(json.dumps(res))
