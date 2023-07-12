a8f32f686db371e83569a91eb9b66292decaed78
Uri Berger <uriber@cs.huji.ac.il>
2023-07-12 13:04:18 +0300

    Fixed a typo

--- mPLUG/scripts/infer_script.sh
+++ mPLUG/scripts/infer_script.sh
@@ -2,13 +2,15 @@
 
 CHECKPOINT=$1
 OUTPUT_DIR=$2
+#CHECKPOINT=output/automatic/after_batch_20_reformulations/checkpoint_00.pth
+#OUTPUT_DIR=auto_re
 echo "Checkpoint=$CHECKPOINT"
 echo "Output dir=$OUTPUT_DIR"
 
 CUDA_VISIBLE_DEVICES=0 venv/bin/python caption_mplug.py \
 --config ./configs/caption_mplug_base.yaml \
---output_dir $CHECKPOINT \
---checkpoint $OUTPUT_DIR \
+--output_dir $OUTPUT_DIR \
+--checkpoint $CHECKPOINT \
 --text_encoder bert-base-uncased \
 --text_decoder bert-base-uncased \
 --min_length 8 \
