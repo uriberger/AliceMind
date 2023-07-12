56fe2cb6f06a6bbf6cff51f385d0cb874593441e
Uri Berger <uriber@cs.huji.ac.il>
2023-07-12 11:39:10 +0300

    Changed the infer bash script to be more convinient

--- mPLUG/scripts/infer_script.sh
+++ mPLUG/scripts/infer_script.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-echo "Checkpoint=$1"
-echo "Output dir=$2"
+CHECKPOINT=$1
+OUTPUT_DIR=$2
+echo "Checkpoint=$CHECKPOINT"
+echo "Output dir=$OUTPUT_DIR"
 
 CUDA_VISIBLE_DEVICES=0 venv/bin/python caption_mplug.py \
 --config ./configs/caption_mplug_base.yaml \
---output_dir $2 \
---checkpoint $1 \
+--output_dir $CHECKPOINT \
+--checkpoint $OUTPUT_DIR \
 --text_encoder bert-base-uncased \
 --text_decoder bert-base-uncased \
 --min_length 8 \
