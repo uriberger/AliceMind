a5771dca66d53d7e751117b9b3b8d47dda680dc1
Uri Berger <uriber@cs.huji.ac.il>
2023-12-18 12:33:57 +0200

    Fixed style training script

--- mPLUG/scripts/vqa_mplug_large_style.sh
+++ mPLUG/scripts/vqa_mplug_large_style.sh
@@ -2,12 +2,9 @@
 
 venv/bin/python vqa_mplug.py \
     --config ./configs/vqa_mplug_large_style.yaml \
-    --output_dir output/vqa_mplug_large_style \
+    --output_dir /cs/labs/oabend/uriber/romantic_reformulation_output \
     --checkpoint ./mplug_large.pth \
     --do_two_optim \
     --add_object \
     --max_input_length 80 \
-    --do_amp \
-    --add_ocr \
-    --deepspeed \
-    --deepspeed_config ./configs/ds_config.json
+    --add_ocr
